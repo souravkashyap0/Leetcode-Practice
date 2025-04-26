75bd562760238ca284a08460ae314d67ac8d761d
Sourav N <55662036+souravkashyap0@users.noreply.github.com>
2025-04-26 17:18:23 +0530

    Time: 1 ms (61.77%) | Memory: 58.2 MB (61.47%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.js
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -3,14 +3,6 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    // let arr =[];
-    // for(let i=0;i<nums.length;i++){
-    //     arr.push(nums[i]);
-    // }
-    // for(let j=0;j<nums.length;j++){
-    //     arr.push(nums[j])
-    // }
-    // return arr;
     nums.push(...nums);
     return  nums;
 };
