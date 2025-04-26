a0a99671f2cb7cda73eca24eb63696bd16b85e84
Sourav N <55662036+souravkashyap0@users.noreply.github.com>
2025-04-26 17:25:49 +0530

    Time: 9 ms (89.28%) | Memory: 73.4 MB (13.55%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+    let result = new Set();
+    for(let i=0;i<nums.length;i++){
+        if(result.has(nums[i])){
+            return true;
+        }
+        result.add(nums[i]);
+    }
+    return false;
+};
