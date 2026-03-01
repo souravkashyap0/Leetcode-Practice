e54864b97a1c96b03006389e581cbcda57e515dd
Sourav N <55662036+souravkashyap0@users.noreply.github.com>
2026-03-01 20:49:46 +0530

    Time: 3 ms (58.78%) | Memory: 55.7 MB (28.21%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,13 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let  twoSumMap = new Map();
-    for(let i =0;i<nums.length;i++){
-        let diff = target - nums[i];
-        if(twoSumMap.has(diff)){
-            return [twoSumMap.get(diff),i];
-        }
-        twoSumMap.set(nums[i],i);
+    let twoSumMap = new Map();
+    for(let i=0;i<nums.length;i++){
+    let match = target - nums[i];
+    if(twoSumMap.has(match)){
+        return [twoSumMap.get(match),i]
     }
-    
+     twoSumMap.set(nums[i],i);
+    }
 };
